50907953ceed67f863a13e3e7159aa8d304211fe
sanket soanawane <154777847+Sanket2321@users.noreply.github.com>
2025-08-01 20:51:56 +0530

    Time: 49 ms (11.25%) | Memory: 45 MB (63.95%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,40 +1,28 @@
-//first method
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans= new int[2];
-
-        for(int i =0; i < nums.length - 1; i++) {
-            for(int j = i + 1 ; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    ans[0] = i;
-                    ans[1] = j;
-                }
-            } 
-        }
-
-        return ans;
-    }
-}
-
-
-
-
-//second method
-/*class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-            int r_n=target-nums[i];
-            if(hm.containsKey(r_n))
-            {
-                int arr[]={hm.get(r_n),i};
-                return arr;
-            }
-            hm.put(nums[i],i);
-
-        }
-        return null;
         
-    }
-} */
+ 
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//              if(nums[i]+nums[j]==target){
+//                 return new int[]{i,j};
+
+//             }
+//         }
+//     }
+//    return new int[]{};
+
+     int ans[]=new int[2];
+
+     for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                ans[0]=i;
+                ans[1]=j;
+            }
+        }
+     }
+     return ans;
+
+}
+}
