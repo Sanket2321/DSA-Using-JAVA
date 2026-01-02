473f94aa547fa4ab7ba24cd8deae48db08653e38
sanket sonawane <154777847+Sanket2321@users.noreply.github.com>
2026-01-02 13:12:30 +0530

    Time: 1 ms (99.80%) | Memory: 47.5 MB (91.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,22 @@
-class Solution{
- public void moveZeroes(int[]nums){
-  int lastNonZeroIndex=0;
-  for(int i=0;i<nums.length;i++){
-   if(nums[i]!=0){
-    nums[lastNonZeroIndex++]=nums[i];
-   }
-  }
-  for(int i=lastNonZeroIndex;i<nums.length;i++){
-   nums[i]=0;
-  }
- }
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int n=nums.length;
+        int zeros[]=new int[n];
+        int index=0;
+        for(int i=0;i<n;i++){
+         
+            if(nums[i]!=0){
+             zeros[index]=nums[i];
+            index++;
+
+            }
+
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=zeros[i];
+        }
+
+        
+    }
 }
